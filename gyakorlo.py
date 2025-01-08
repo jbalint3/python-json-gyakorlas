70a3d19844347c3621e76b84c56a05c8acc482ef
jbalint3 <146117606+jbalint3@users.noreply.github.com>
2025-01-08 10:14:55 +0100

    Kezdet

---
+++ gyakorlo.py
@@ -0,0 +1,10 @@
+#generálj egy véletlen számot
+#kérj be a felhasználótol egy számot
+#írd ki hogy a beírt szám kiusebb-e vagy nagyobb-e mint a véletlen szám
+#legyenez ismételve amíg el nem találja
+
+
+#ismétlés:
+tomb = [2,7,3,5,1,9,4]
+#irasd ki a legkisebb elemet
+#írasd ki a páros számokat
